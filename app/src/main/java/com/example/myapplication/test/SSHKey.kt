e85cdfc363b9eba673945dd8c314f9734852c1d0
chandragupt <chandraguptmehata@gmail.com>
2024-05-10 13:32:31 +0530

    multiple ssh key

--- app/src/main/java/com/example/myapplication/test/SSHKey.kt
+++ app/src/main/java/com/example/myapplication/test/SSHKey.kt
@@ -1,8 +1,7 @@
 package com.example.myapplication.test
 
 /**
- * https://www.youtube.com/watch?v=R8_veQiYBjI
  * https://gist.github.com/jexchan/2351996
  */
-class GithubAction {
+class SSHKey {
 }
